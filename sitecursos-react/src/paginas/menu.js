01c6b2ac9d204c57e2c8d3880ed59c5d3ecb083d
Allane <a.emanoelle21@gmail.com>
2025-02-24 22:46:54 -0300

    'atualização'

---
+++ sitecursos-react/src/paginas/menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Menu =()=>{
+    return(
+        <div className='menu'>
+            <nav>DJSHJ</nav>
+            <h1>xdnj</h1>
+        </div>
+    );
+}
+export default Menu;
